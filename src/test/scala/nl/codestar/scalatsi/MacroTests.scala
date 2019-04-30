217d9e7c24feac52ea0ce8e2256c23d77ab3c293
David Hoepelman <dj.hoepelman@portofrotterdam.com>
2019-04-30 22:11:58 +0200

    Add tests for sealed traits

--- src/test/scala/nl/codestar/scalatsi/MacroTests.scala
+++ src/test/scala/nl/codestar/scalatsi/MacroTests.scala
@@ -6,7 +6,7 @@ import nl.codestar.scalatsi.TypescriptType._
 case class Person(name: String, age: Int)
 
 class MacroTests extends FlatSpec with Matchers with DefaultTSTypes {
-  // Scala 2.11.11 (maybe others) give false positive unused warnings if a class is used only as a generic
+  // Scala 2.11.11 (maybe others) give false positive unused warnings if a class is used only as a generic parameter
   def ignoreUnused(o: Object) = ()
 
   "The case class to TypeScript type macro" should "be able to translate a simple case class" in {
@@ -30,6 +30,26 @@ class MacroTests extends FlatSpec with Matchers with DefaultTSTypes {
     implicit val tsA: TSIType[A] = TSType.fromCaseClass
 
     TSType.fromCaseClass[B] shouldBe TSType.interface("IB", "a" -> tsA.get)
+  }
+
+  it should "handle sealed traits" in {
+    sealed trait AB
+    case class A(a: Int) extends AB
+    case class B(b: String) extends AB
+
+    // implicit val abType: TSType[AB] = ???
+    pending
+  }
+
+  it should "handle recursive definitions" in {
+    sealed trait LinkedList
+    case object Nil extends LinkedList
+    case class Node(value: Int, next: LinkedList = Nil)
+
+    // TODO: Determine this automatically: [#35](https://github.com/code-star/scala-tsi/issues/35)
+    implicit val llType: TSType[LinkedList] = TSType(TSType.external("INil").get | TSType.external("INode").get)
+
+    TSType.fromCaseClass[Node] shouldBe TSType.interface("INode", "value" -> TSNumber, "next" -> llType.get)
   }
 
   it should "not compile if a nested definition is missing" in {
