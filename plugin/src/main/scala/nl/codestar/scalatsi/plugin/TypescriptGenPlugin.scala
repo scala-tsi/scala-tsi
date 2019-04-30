e0ab36907009e4cd2f4b778b682cd2a7a0c55985
David Hoepelman <dj.hoepelman@portofrotterdam.com>
2019-04-30 20:29:34 +0200

    Formatting for some missed file

--- plugin/src/main/scala/nl/codestar/scalatsi/plugin/TypescriptGenPlugin.scala
+++ plugin/src/main/scala/nl/codestar/scalatsi/plugin/TypescriptGenPlugin.scala
@@ -8,13 +8,13 @@ object TypescriptGenPlugin extends AutoPlugin {
   object autoImport {
     val generateTypescript =
       taskKey[Unit]("Generate typescript this project")
-    val generateTypescriptGeneratorApplication = taskKey[Seq[File]]("Generate an application that will generate typescript from the classes that are configured")
+    val generateTypescriptGeneratorApplication =
+      taskKey[Seq[File]]("Generate an application that will generate typescript from the classes that are configured")
     val typescriptClassesToGenerateFor =
       settingKey[Seq[String]]("Classes to generate typescript interfaces for")
-    val typescriptGenerationImports = settingKey[Seq[String]](
-      "Additional imports (i.e. your packages so you don't need to prefix your classes)")
-    val typescriptOutputFile = settingKey[File](
-      "File where all typescript interfaces will be written to")
+    val typescriptGenerationImports =
+      settingKey[Seq[String]]("Additional imports (i.e. your packages so you don't need to prefix your classes)")
+    val typescriptOutputFile = settingKey[File]("File where all typescript interfaces will be written to")
   }
 
   import autoImport._
@@ -35,22 +35,21 @@ object TypescriptGenPlugin extends AutoPlugin {
       typescriptGenerationImports.value,
       typescriptClassesToGenerateFor.value,
       sourceManaged.value,
-      typescriptOutputFile.value),
-    sourceGenerators in Compile += generateTypescriptGeneratorApplication in Compile,
+      typescriptOutputFile.value
+    ),
+    sourceGenerators in Compile += generateTypescriptGeneratorApplication in Compile
   )
 
   def createTypescriptGenerationTemplate(
-      imports: Seq[String],
-      typesToGenerate: Seq[String],
-      sourceManaged: File,
-      typescriptOutputFile: File): Seq[File] = {
+    imports: Seq[String],
+    typesToGenerate: Seq[String],
+    sourceManaged: File,
+    typescriptOutputFile: File
+  ): Seq[File] = {
     val targetFile = sourceManaged / "nl" / "codestar" / "scala" / "ts" / "generator" / "ApplicationTypescriptGeneration.scala"
 
     val toWrite: String = txt
-      .generateTypescriptApplicationTemplate(
-        imports,
-        typesToGenerate,
-        typescriptOutputFile.getAbsolutePath)
+      .generateTypescriptApplicationTemplate(imports, typesToGenerate, typescriptOutputFile.getAbsolutePath)
       .body
       .stripMargin
 
