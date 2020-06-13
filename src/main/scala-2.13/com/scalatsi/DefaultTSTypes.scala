8dd21be095a6294f69b1f0e305b3f8e12f608628
David Hoepelman <992153+dhoepelman@users.noreply.github.com>
2020-06-13 22:51:31 +0200

    Upgrade scalafmt and set indentation

--- src/main/scala-2.13/com/scalatsi/DefaultTSTypes.scala
+++ src/main/scala-2.13/com/scalatsi/DefaultTSTypes.scala
@@ -3,10 +3,10 @@ package com.scalatsi
 /** The default Scala-to-Typescript mappings provided by this library */
 // Cannot be put in the scala-agnostic code because 2.12 does not support literal types
 trait DefaultTSTypes
-    extends PrimitiveTSTypes
-    with ScalaTSTypes
-    with CollectionTSTypes
-    with TupleTSTypes
-    with JavaTSTypes
-    with LiteralTSTypes {}
+  extends PrimitiveTSTypes
+  with ScalaTSTypes
+  with CollectionTSTypes
+  with TupleTSTypes
+  with JavaTSTypes
+  with LiteralTSTypes {}
 object DefaultTSTypes extends DefaultTSTypes
