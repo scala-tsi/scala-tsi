7baaa0c89051c9d850fe08bb98f835d86017e923
David Hoepelman <dhoepelman@xebia.com>
2021-02-26 17:16:05 +0100

    nowarn

--- src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
+++ src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
@@ -4,10 +4,9 @@ import TypescriptType._
 import scala.annotation.nowarn
 
 trait PrimitiveTSTypes {
-  implicit val booleanTsType: TSType[Boolean]      = TSType(TSBoolean)
-  implicit val stringTsType: TSType[String]        = TSType(TSString)
-  @nowarn("cat=unused-params")
-  implicit def numberTsType[T: Numeric]: TSType[T] = TSType(TSNumber)
+  implicit val booleanTsType: TSType[Boolean]              = TSType(TSBoolean)
+  implicit val stringTsType: TSType[String]                = TSType(TSString)
+  @nowarn implicit def numberTsType[T: Numeric]: TSType[T] = TSType(TSNumber)
 }
 
 object PrimitiveTSTypes extends PrimitiveTSTypes
