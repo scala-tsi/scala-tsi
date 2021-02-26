794ab15224fe8c9ca0a5ee5be1bef96151e25d0d
David Hoepelman <dhoepelman@xebia.com>
2021-02-26 17:01:14 +0100

    Fix new scala 2.13.5 errors

--- src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
+++ src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
@@ -1,10 +1,12 @@
 package com.scalatsi
 
 import TypescriptType._
+import scala.annotation.nowarn
 
 trait PrimitiveTSTypes {
   implicit val booleanTsType: TSType[Boolean]      = TSType(TSBoolean)
   implicit val stringTsType: TSType[String]        = TSType(TSString)
+  @nowarn("cat=unused")
   implicit def numberTsType[T: Numeric]: TSType[T] = TSType(TSNumber)
 }
 
