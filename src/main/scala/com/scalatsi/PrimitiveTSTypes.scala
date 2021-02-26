93191a793ed2c182f702a37bea26a7ffc8f79858
David Hoepelman <dhoepelman@xebia.com>
2021-02-26 17:32:02 +0100

    Make scala version dynamic

--- src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
+++ src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
@@ -6,7 +6,7 @@ import scala.annotation.nowarn
 trait PrimitiveTSTypes {
   implicit val booleanTsType: TSType[Boolean]              = TSType(TSBoolean)
   implicit val stringTsType: TSType[String]                = TSType(TSString)
-  @nowarn implicit def numberTsType[T: Numeric]: TSType[T] = TSType(TSNumber)
+  @nowarn("cat=unused-params") implicit def numberTsType[T: Numeric]: TSType[T] = TSType(TSNumber)
 }
 
 object PrimitiveTSTypes extends PrimitiveTSTypes
