834191e8fc69c2a026cd3e1c9870cf4ca4394f1f
David Hoepelman <dhoepelman@xebia.com>
2021-02-26 17:12:57 +0100

    Fix unused

--- src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
+++ src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
@@ -6,7 +6,7 @@ import scala.annotation.nowarn
 trait PrimitiveTSTypes {
   implicit val booleanTsType: TSType[Boolean]      = TSType(TSBoolean)
   implicit val stringTsType: TSType[String]        = TSType(TSString)
-  @nowarn("cat=unused")
+  @nowarn("cat=unused-params")
   implicit def numberTsType[T: Numeric]: TSType[T] = TSType(TSNumber)
 }
 
