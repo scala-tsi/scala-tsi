305af5c2c49340b7f622e9836b33961263393242
David Hoepelman <dhoepelman@xebia.com>
2021-02-26 17:55:50 +0100

    Fix linting

--- src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
+++ src/main/scala/com/scalatsi/PrimitiveTSTypes.scala
@@ -4,8 +4,8 @@ import TypescriptType._
 import scala.annotation.nowarn
 
 trait PrimitiveTSTypes {
-  implicit val booleanTsType: TSType[Boolean]              = TSType(TSBoolean)
-  implicit val stringTsType: TSType[String]                = TSType(TSString)
+  implicit val booleanTsType: TSType[Boolean]                                   = TSType(TSBoolean)
+  implicit val stringTsType: TSType[String]                                     = TSType(TSString)
   @nowarn("cat=unused-params") implicit def numberTsType[T: Numeric]: TSType[T] = TSType(TSNumber)
 }
 
