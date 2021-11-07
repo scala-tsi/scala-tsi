6ef9ea3875c3fa95a1614ed06c85ad697f52d9d5
David Hoepelman <992153+dhoepelman@users.noreply.github.com>
2021-11-07 16:21:41 +0100

    Add Either to nested generation tests

--- plugin/src/sbt-test/ScalaTsiPlugin/generation-tests/src/main/scala/models/NestedGenerated.scala
+++ plugin/src/sbt-test/ScalaTsiPlugin/generation-tests/src/main/scala/models/NestedGenerated.scala
@@ -6,9 +6,14 @@ case class A(field: String)
 
 case class B(a: A)
 
+case class C(field: Int)
+
 case class NestedGenerated(
     direct: A,
     indirect: B,
     inList: Seq[A],
-    inDoubleList: Seq[Seq[A]]
+    inDoubleList: Seq[Seq[A]],
+    inEither: Either[Int, C],
+    inTuple: (A, Int, String),
+    deepNested: Either[A, Either[Int, B]]
 )
