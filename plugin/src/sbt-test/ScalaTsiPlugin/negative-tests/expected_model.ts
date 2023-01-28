02a0c9b001d925d5e43609fc57aff819f68565a3
David Hoepelman <992153+dhoepelman@users.noreply.github.com>
2023-01-28 11:32:45 +0100

    Fix expected_model in negative-tests

--- plugin/src/sbt-test/ScalaTsiPlugin/negative-tests/expected_model.ts
+++ plugin/src/sbt-test/ScalaTsiPlugin/negative-tests/expected_model.ts
@@ -1,7 +1,7 @@
 // DO NOT EDIT: generated file by scala-tsi
 
 export interface IClassWithUndefinedMember {
-  notDefined: "Could not find TSType[models.NotDefined] in scope and could not generate it."
+  notDefined: "Could not find TSType[models.NotDefined] in scope and could not generate it"
 }
 
 export interface IRecursiveA {
